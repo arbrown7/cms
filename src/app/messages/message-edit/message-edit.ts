bf55c619405e3f12e7364a2678fc7cd61d180c96
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-22 15:45:54 -0700

    Modify the MessageEditComponent to send a new message
    W03 Assign.

--- src/app/messages/message-edit/message-edit.ts
+++ src/app/messages/message-edit/message-edit.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Output, ElementRef, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.css',
 })
 export class MessageEdit {
+  @ViewChild('subject') subjectRef: ElementRef;
+  @ViewChild('msgText') msgTextRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Autumn';
 
+  onSendMessage() {
+    //Get the value stored in the subject input element
+    const currentSubject = this.subjectRef.nativeElement.value;
+    //Get the value stored in the msgText input element
+    const currentMsgText = this.msgTextRef.nativeElement.value;
+    //Create a new Message object
+    //Assign a hardcoded number to the id property of the new Message object
+    //Assign the value of the currentSender class variable to the sender property of the new Message object
+    //Assign the values retrieved from the subject and msgText input elements to the corresponding properties of the new Message object
+    const newMessage = new Message(1, currentSubject, currentMsgText, this.currentSender);
+    //Call the addMessageEvent emitter’s emit() method and pass it the new Message object just created
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectRef.nativeElement.value = '';
+    this.msgTextRef.nativeElement.value = '';
+  }
 }
