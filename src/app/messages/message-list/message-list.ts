86bea954f11cd8bd67f911e345191c356bf0a961
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-01-22 16:24:33 -0700

    Display and add messages to the MessageList component
    Not quite finished yet...

--- src/app/messages/message-list/message-list.ts
+++ src/app/messages/message-list/message-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.css',
 })
 export class MessageList {
+  messages: Message[] = [
+    new Message(2, 'Subject 2', 'Message 2', 'Sender 2'),
+    new Message(3, 'Subject 3', 'Message 3', 'Sender 3'),
+    new Message(4, 'Subject 4', 'Message 4', 'Sender 4')
+  ]
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
