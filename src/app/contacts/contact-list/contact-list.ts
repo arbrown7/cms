d29c5cd2d54d248788680990ea1e0587a92cb9d2
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-04 14:00:34 -0700

    Injecting and Using the Contact Service
    W5 Assignment

--- src/app/contacts/contact-list/contact-list.ts
+++ src/app/contacts/contact-list/contact-list.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
@@ -7,12 +8,17 @@ import { Contact } from '../contact.model';
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.css',
 })
-export class ContactList {
+export class ContactList implements OnInit{
   @Output() selectedContactEvent = new EventEmitter<Contact>();
-  contacts: Contact[] = [
-    new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "assets/images/jacksonk.jpg"),
-    new Contact(2, "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "assets/images/barzeer.jpg"),
-  ];
+  contacts: Contact[] = [];
+
+  constructor(private contactService: ContactService) {
+
+  }
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+  }
 
   onSelected(contact: Contact) {
     this.selectedContactEvent.emit(contact);
