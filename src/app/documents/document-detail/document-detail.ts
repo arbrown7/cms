8ebd984510bae59484c906fb1a88f81c7203f291
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-11 10:45:47 -0700

    9. Implement the View Button
    W06 Assignment

--- src/app/documents/document-detail/document-detail.ts
+++ src/app/documents/document-detail/document-detail.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { WindRefService } from '../../wind-ref.service';
 
 @Component({
   selector: 'cms-document-detail',
@@ -12,10 +13,12 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class DocumentDetail implements OnInit {
   selectedDocument: Document;
   id: string;
+  nativeWindow: any;
 
   constructor(private documentService: DocumentService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private windRefService: WindRefService) {
 
   }
 
@@ -27,5 +30,12 @@ export class DocumentDetail implements OnInit {
         this.selectedDocument = this.documentService.getDocument(this.id);
       }
     )
+    this.nativeWindow = this.windRefService.getNativeWindow();
+  }
+
+  onView() {
+    if (this.selectedDocument.url) {
+      this.nativeWindow.open(this.selectedDocument.url)
+    }
   }
 }
