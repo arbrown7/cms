b56312107e75d885387e922dba9944fee4faeeb3
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-11 09:56:24 -0700

    5. Define the Child Routes for the Document Sub-Components
    W06 Assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from "@angular/router";
 import { Documents } from "./documents/documents";
 import { MessageList } from "./messages/message-list/message-list";
 import { Contacts } from "./contacts/contacts";
+import { DocumentEdit } from "./documents/document-edit/document-edit";
+import { DocumentDetail } from "./documents/document-detail/document-detail";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/documents', pathMatch: 'full'},
-    { path: 'documents', component: Documents},
+    { path: 'documents', component: Documents, children: [
+        { path: 'new', component: DocumentEdit},
+        { path: ':id', component: DocumentDetail},
+        { path: ':id/edit', component: DocumentEdit}
+    ] },
     { path: 'messages', component: MessageList},
     { path: 'contacts', component: Contacts}
 ];
