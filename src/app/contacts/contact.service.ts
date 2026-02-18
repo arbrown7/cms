48cb16ab4aedcc7e56e581aae639281ebbbe07e6
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-17 21:42:55 -0700

    Create Functions to Add, Update and Delete Contacts
    W07 Assignment

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Contact } from './contact.model';
 import { MOCKCONTACTS } from './MOCKCONTACTS';
-import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,13 @@ import { Subject } from 'rxjs';
 export class ContactService {
   contacts : Contact[] = [];
   contactSelectedEvent = new EventEmitter<Contact>();
-  contactChangedEvent = new EventEmitter<Contact[]>();
+  //contactChangedEvent = new EventEmitter<Contact[]>();
   contactListChangedEvent = new Subject<Contact[]>();
+  maxContactId: number;
 
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
   }
 
   getContacts(): Contact[] {
@@ -40,7 +43,46 @@ export class ContactService {
         return;
     }
     this.contacts.splice(pos, 1);
-    //not sure if this is supposed to be changed yet
     this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+  getMaxId() {
+    let maxId = 0;
+
+    for (const contact of this.contacts) {
+      let currentId = parseInt(contact.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+
+    return maxId;
+  }
+
+    addContact(newContact: Contact) {
+      if (!newContact) {
+        return;
+      }
+    
+      this.maxContactId++;
+  
+      newContact.id = this.maxContactId.toString();
+      this.contacts.push(newContact);
+      this.contactListChangedEvent.next(this.contacts.slice());
+    }
+  
+    updateDocument(originalContact: Contact, newContact: Contact) {
+      if (!originalContact || !newContact) {
+        return;
+      }
+  
+      let pos = this.contacts.indexOf(originalContact);
+      if (pos < 0 ) {
+        return;
+      }
+  
+      newContact.id = originalContact.id;
+      this.contacts[pos] = newContact; 
+      this.contactListChangedEvent.next(this.contacts.slice());
     }
 }
