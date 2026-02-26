baa79af9701c50d1e05a7fdf98e5274184fa1405
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-26 16:16:26 -0700

    Implementing the Behavior of the Document Edit Form
    W08 Assignment

--- src/app/documents/document-edit/document-edit.ts
+++ src/app/documents/document-edit/document-edit.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Params } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-document-edit',
@@ -8,15 +13,51 @@ import { Document } from '../document.model';
   styleUrl: './document-edit.css',
 })
 export class DocumentEdit implements OnInit {
-  originalDocuent: Document;
+  originalDocument: Document;
   document: Document;
   editMode: boolean = false;
+  id: number;
+
+  constructor(
+            private documentService: DocumentService,
+            private router: Router,
+            private route: ActivatedRoute) {
+
+}
   ngOnInit(): void {
-    
+    this.route.params.subscribe (
+     (params: Params) => {
+        this.id = params['id'];
+
+        if (!this.id) {
+          this.editMode = false;
+          return;
+        }
+
+        this.originalDocument = this.documentService.getDocument(this.id.toString());
+
+        if(!this.originalDocument) {
+          return;
+        }
+        
+        this.editMode = true;
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+   }); 
   }
 
-  onCancel() {}
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route})
+  }
 
-  onSubmit(f) {}
+  onSubmit(form: NgForm) {
+    //Assign the values in the form fields to the corresponding properties in the newDocument
+    if (this.editMode === true) {
+      this.documentService.updateDocument(this.originalDocument, form.value);
+    } else {
+      this.documentService.addDocument(form.value);
+    }
+   //route back to the '/documents' URL 
+   this.router.navigate(['../'], {relativeTo: this.route})
+  }
 
 }
