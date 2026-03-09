e26fc7e7446dee2e64fede176ea26ec6f590e737
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-03-09 16:28:48 -0600

    Create the Web Services for Your Application
    W10 Assignment

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var logger = require('morgan');
 
 // import the routing file to handle the default (index) route
 var index = require('./server/routes/app');
-
-// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ... 
+const messageRoutes = require('./server/routes/messages');
+const contactRoutes = require('./server/routes/contacts');
+const documentRoutes = require('./server/routes/documents');
 
 var app = express(); // create an instance of express
 
@@ -42,8 +43,9 @@ app.use(express.static(path.join(__dirname, 'dist/cms/browser')));
 
 // Tell express to map the default route ('/') to the index route
 app.use('/', index);
-
-// ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
+app.use('/messages', messageRoutes);
+app.use('/contacts', contactRoutes);
+app.use('/documents', documentRoutes);
 
 // Tell express to map all other non-defined routes back to the index page
 app.get('/*path', (req, res) => {
