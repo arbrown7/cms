baccc767147b90ec789bf919150ab559064961b9
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-03-17 15:18:49 -0600

    Implement the Messages Routing File
    W11 Assignment

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,3 +1,99 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 module.exports = router; 
+
+const sequenceGenerator = require('./sequenceGenerator');
+const Message = require('../models/message');
+
+router.get('/', (req, res, next) => {
+   Message.find()
+    .then(foundMessages => {
+        res.status(200).json({
+        message: 'Messages found successfully',
+        messages: foundMessages
+      });
+    })
+    .catch(error => {
+       res.status(500).json({
+          message: 'An error occurred',
+          error: error
+        });
+    });   
+});
+
+router.post('/', (req, res, next) => {
+  const maxMessageId = sequenceGenerator.nextId("messages");
+
+  const message = new Message({
+    id: maxMessageId,
+    subject: req.body.subject,
+    msgText: req.body.message,
+    sender: new mongoose.Types.ObjectId("69b193189458c860eb9b4cae")
+  });
+
+  message.save()
+    .then(createdMessage => {
+      res.status(201).json({
+        message: 'Message added successfully',
+        message: createdMessage
+      });
+    })
+    .catch(error => {
+       res.status(500).json({
+          message: 'An error occurred',
+          error: error
+        });
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  Message.findOne({ id: req.params.id })
+    .then(message => {
+      message.subject = req.body.subject;
+      message.msgTxt = req.body.message;
+
+      Message.updateOne({ id: req.params.id }, message)
+        .then(result => {
+          res.status(204).json({
+            message: 'Message updated successfully'
+          })
+        })
+        .catch(error => {
+           res.status(500).json({
+           message: 'An error occurred',
+           error: error
+         });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Message not found.',
+        error: { message: 'Message not found'}
+      });
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Message.findOne({ id: req.params.id })
+    .then(message => {
+      Message.deleteOne({ id: req.params.id })
+        .then(result => {
+          res.status(204).json({
+            message: "Message deleted successfully"
+          });
+        })
+        .catch(error => {
+           res.status(500).json({
+           message: 'An error occurred',
+           error: error
+         });
+        })
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Message not found.',
+        error: { message: 'Message not found'}
+      });
+    });
+});
